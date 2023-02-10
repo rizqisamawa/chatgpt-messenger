bd4b8753785362a78df3202afb5ce43a629849f1
rizqisamawa <rizqisamawa11@gmail.com>
2023-02-10 13:14:04 +0700

    config: auth google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,34 @@
-import SideBar from "../components/SideBar";
 import "../styles/globals.css";
+import { getServerSession } from "next-auth";
+import { SessionProvider } from "../components/SessionProvider";
+import SideBar from "../components/SideBar";
+import Login from "../components/Login";
+import { authOptions } from "../pages/api/auth/[...nextAuth]";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html>
       <head />
       <body>
-        <div className="flex">
-          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
-            <SideBar />
-          </div>
-          {/* ChatProvider - Notification */}
-          <div className="bg-[#343541] flex-1">{children}</div>
-        </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                <SideBar />
+              </div>
+              {/* ChatProvider - Notification */}
+              <div className="bg-[#343541] flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
